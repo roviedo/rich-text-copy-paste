92da31fb5e38ec25c3fd5844ff17132dbff8a0a3
roviedo <raul.oviedo@ipsoft.com>
2021-04-07 22:46:59 -0400

    initial commit added input field and testing clipboarddata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [textInput, handleChange] = useState('');
+  // const handlePaste = (e) => {
+  //   var clipboardData, pastedData;
+  //   console.log('e', e);
+
+  //   // Stop data actually being pasted into div
+  //   e.stopPropagation();
+  //   e.preventDefault();
+
+  //   // Get pasted data via clipboard API
+  //   clipboardData = e.clipboardData || window.clipboardData;
+  //   // pastedData = clipboardData.getData('Text');
+    
+  //   // Do whatever with pasteddata
+  //   // alert(pastedData);
+  //   console.log('im not here');
+  //   return handleChange('test');
+  // }
+  const handlePaste = (e) => {
+    var clipboardData, pastedData;
+    clipboardData = e.clipboardData || window.clipboardData;
+    console.log('clipboardData', clipboardData);
+    pastedData = clipboardData.getData('text/html');
+    console.log('pastedData', pastedData);
+    return handleChange(e.target.value);
+  }
+  const handleOnChange = (e) => handleChange(e.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          onPaste={handlePaste}
+          onChange={handleOnChange} />
+      </div>
     </div>
   );
 }
