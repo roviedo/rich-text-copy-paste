399bc69560c2584e1735ab43562884278c143792
roviedo <raul.oviedo@ipsoft.com>
2021-04-08 00:15:43 -0400

    Can now add rich text and display below  as html

--- src/App.js
+++ src/App.js
@@ -4,40 +4,33 @@ import './App.css';
 
 function App() {
   const [textInput, handleChange] = useState('');
-  // const handlePaste = (e) => {
-  //   var clipboardData, pastedData;
-  //   console.log('e', e);
+  const [richText, handleRichText] = useState('');
+  const [showRichText, handleShowRichText] = useState(false);
 
-  //   // Stop data actually being pasted into div
-  //   e.stopPropagation();
-  //   e.preventDefault();
-
-  //   // Get pasted data via clipboard API
-  //   clipboardData = e.clipboardData || window.clipboardData;
-  //   // pastedData = clipboardData.getData('Text');
-    
-  //   // Do whatever with pasteddata
-  //   // alert(pastedData);
-  //   console.log('im not here');
-  //   return handleChange('test');
-  // }
   const handlePaste = (e) => {
     var clipboardData, pastedData;
     clipboardData = e.clipboardData || window.clipboardData;
-    console.log('clipboardData', clipboardData);
     pastedData = clipboardData.getData('text/html');
-    console.log('pastedData', pastedData);
-    return handleChange(e.target.value);
+    handleChange(e.target.value);
+    handleRichText(pastedData);
+    handleShowRichText(false);
   }
-  const handleOnChange = (e) => handleChange(e.target.value);
+  const handleOnChange = (e) => {
+    handleChange(e.target.value);
+    handleShowRichText(false);
+  }
+  const handleSubmit = () => handleShowRichText(true);
 
   return (
     <div className="App">
-      <div>
-        <input
+      <div className="textarea-button-container">
+        <textarea
           type="text"
           onPaste={handlePaste}
-          onChange={handleOnChange} />
+          onChange={handleOnChange}
+        />
+        <button type="submit" onClick={handleSubmit} >Submit</button>
+        { showRichText ? <div dangerouslySetInnerHTML={{__html: richText}} className="rich-text" /> : null }
       </div>
     </div>
   );
